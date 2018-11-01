cdd4f796e952a2c1a314769811f03599e59e3ffb
muroqiu <muroqiu@qq.com>
2018-11-01 12:06:29 +0800

    完善测试页面

--- sample/src/main/java/com/scrm/assistant/MainActivity.java
+++ sample/src/main/java/com/scrm/assistant/MainActivity.java
@@ -6,15 +6,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnTest;
+    TextView tvDebug;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        btnTest = findViewById(R.id.btnTest);
+        tvDebug = findViewById(R.id.tvDebug);
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvDebug.setText("abc");
             }
         });
     }
