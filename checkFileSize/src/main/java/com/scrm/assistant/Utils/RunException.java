fd0c69ea91b644a3b6c870698f48f4e039e4565d
muroqiu <muroqiu@qq.com>
2019-06-05 17:09:59 +0800

    增加shell相关类

---
+++ checkFileSize/src/main/java/com/scrm/assistant/Utils/RunException.java
@@ -0,0 +1,11 @@
+package com.scrm.assistant.Utils;
+
+public class RunException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public RunException(String detailMessage) {
+        super(detailMessage);
+    }
+
+}
