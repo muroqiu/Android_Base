ef947277fce7aa497f1694a2ef9bf2bd6a4f86ca
muroqiu <muroqiu@qq.com>
2019-06-14 17:49:24 +0800

    增加从文件中过滤数据

--- checkFileSize/src/main/java/com/scrm/im/MainActivity.java
+++ checkFileSize/src/main/java/com/scrm/im/MainActivity.java
@@ -1,6 +1,8 @@
 package com.scrm.im;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +13,17 @@ import android.widget.EditText;
 
 import com.scrm.im.Utils.SPUtils;
 import com.scrm.im.Utils.Simulator;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     Button btnTest;
@@ -86,6 +99,92 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.btnClear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                try {
+//                    tvDebug.append(Simulator.execute("rm system/etc/whitelistapps"));
+//                    tvDebug.append(Simulator.execute("rm data/data/scrm/whitelistapps"));
+//                    tvDebug.append(Simulator.execute("rm data/scrm/whitelistapps"));
+//                    tvDebug.append("\n");
+//                } catch (Exception e) {
+//                    Log.e("MainActivity", e.toString());
+//                }
+
+                pickDataFromFile();
+            }
+        });
+
+    }
+
+    /**
+     * 按行读取文件内容
+     *
+     * @param path
+     * @return
+     */
+    public static StringBuilder pickDataLine(String path) {
+        File file = new File(path);
+        if (!file.exists()) {
+            Log.d("FileUtils", "file doesn't exists");
+            return null;
+        }
+        InputStreamReader reader = null;
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            reader = new InputStreamReader(new FileInputStream(file));
+            br = new BufferedReader(reader);
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                if (line.contains("DATA-USAGE")) {
+                    Log.d("FileUtils", line);
+                    String tmp = (line.substring(0,23) + " " + line.substring(line.indexOf("DATA-USAGE")));
+                    sb.append(tmp);
+                    sb.append("\n");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb;
+    }
+
+    protected void pickDataFromFile() {
+//        String sdPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File dirPath = new File("/sdcard/scrm/picks/");
+        File[] files = dirPath.listFiles();
+        //如果为空对象，或为空文件夹，则直接删除文件夹
+        if (files == null || files.length == 0) {
+            return;
+        }
+        File targetFile = new File ("/sdcard/scrm/target.txt");
+        if (targetFile.exists()) {
+            targetFile.delete();
+        }
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(targetFile);
+            for (File itemFile : files) {
+                StringBuilder sb = pickDataLine(itemFile.getAbsolutePath());
+                fos.write(sb.toString().getBytes());
+            }
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
