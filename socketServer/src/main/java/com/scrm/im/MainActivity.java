d3f2e75ed31353d305595b4b0f122fd70d51cb93
OSBill <962237627@qq.com>
2019-10-10 17:09:32 +0800

    完善socketServer工程

--- socketServer/src/main/java/com/scrm/im/MainActivity.java
+++ socketServer/src/main/java/com/scrm/im/MainActivity.java
@@ -1,190 +1,110 @@
 package com.scrm.im;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.os.Environment;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import com.scrm.im.Utils.SPUtils;
-import com.scrm.im.Utils.Simulator;
-
+import android.os.Bundle;
+import android.widget.TextView;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.Map;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnTest;
-    EditText tvDebug;
-    /**
-     * 文件上传缓存对应表
-     */
-    private static SPUtils.Sp mFileCacher_Upload = SPUtils.getSp(SPUtils.SpType.FILE_UPLOAD_CACHER);
+
+    public static ServerSocket serverSocket = null;
+    public static TextView tv_IP, tv_show;
+    private String IP = "";
+    String buffer = "";
+    public static Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(android.os.Message msg) {
+            if (msg.what==0x11) {
+                Bundle bundle = msg.getData();
+// tv_show.setText("client:  "+bundle.getString("msg")+"\n");
+                initJX(bundle.getString("msg"));
+            }
+        };
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        receive();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
 
-        btnTest = findViewById(R.id.btnTest);
-        tvDebug = findViewById(R.id.edtDebug);
-
-        btnTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    private void initView() {
+        tv_show = (TextView) findViewById(R.id.tv_show);
+        tv_IP = (TextView) findViewById(R.id.tv_IP);
+        IP = getlocalip();
+        tv_IP.setText("IP addresss:"+IP);
+    }
+    private void receive(){
+        new Thread() {
+            public void run() {
+                Bundle bundle = new Bundle();
+                bundle.clear();
+                OutputStream output;
+                String str = "hello hehe";
                 try {
-                    tvDebug.append(Simulator.execute("du -sm /sdcard/* | awk '$1 > 100' | sort -nr | head -3"));
-                    tvDebug.append("\n");
-                } catch (Exception e) {
-                    Log.e("MainActivity", e.toString());
+                    serverSocket = new ServerSocket(30000);
+                    while (true) {
+                        Message msg = new Message();
+                        msg.what = 0x11;
+                        try {
+                            Socket socket = serverSocket.accept();
+                            output = socket.getOutputStream();
+                            output.write(str.getBytes("utf-8"));
+                            output.flush();
+                            socket.shutdownOutput();
+                            BufferedReader bff = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            String line = null;
+                            buffer = "";
+                            while ((line = bff.readLine())!=null) {
+                                buffer = line + buffer;
+                            }
+                            bundle.putString("msg", buffer.toString());
+                            msg.setData(bundle);
+                            mHandler.sendMessage(msg);
+                            bff.close();
+                            output.close();
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
                 }
-                try {
-                    tvDebug.append(Simulator.execute("du -sm /sdcard/tencent/MicroMsg/* | awk '$1 > 100' | sort -nr | head -3"));
-                    tvDebug.append("\n");
-                } catch (Exception e) {
-                    Log.e("MainActivity", e.toString());
-                }
-                try {
-                    tvDebug.append(Simulator.execute("du -sm /sdcard/scrm/* | awk '$1 > 100' | sort -nr | head -3"));
-                    tvDebug.append("\n");
-                } catch (Exception e) {
-                    Log.e("MainActivity", e.toString());
-                }
-                tvDebug.append("\n");
-            }
-        });
+            };
+        }.start();
+    }
 
-        findViewById(R.id.btnSet).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    tvDebug.append(Simulator.execute("du -sm /data/data/* | awk '$1 > 100' | sort -nr | head -3"));
-                    tvDebug.append("\n");
-                } catch (Exception e) {
-                    Log.e("MainActivity", e.toString());
-                }
-                try {
-                    tvDebug.append(Simulator.execute("du -sm /data/data/com.tencent.mm/* | awk '$1 > 100' | sort -nr | head -3"));
-                    tvDebug.append("\n");
-                } catch (Exception e) {
-                    Log.e("MainActivity", e.toString());
-                }
-                try {
-                    tvDebug.append(Simulator.execute("du -sm /data/data/com.tencent.mm/MicroMsg/* | awk '$1 > 100' | sort -nr | head -3"));
-                    tvDebug.append("\n");
-                } catch (Exception e) {
-                    Log.e("MainActivity", e.toString());
-                }
-            }
-        });
-
-        findViewById(R.id.btnClear).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                try {
-//                    tvDebug.append(Simulator.execute("rm system/etc/whitelistapps"));
-//                    tvDebug.append(Simulator.execute("rm data/data/scrm/whitelistapps"));
-//                    tvDebug.append(Simulator.execute("rm data/scrm/whitelistapps"));
-//                    tvDebug.append("\n");
-//                } catch (Exception e) {
-//                    Log.e("MainActivity", e.toString());
-//                }
-
-                pickDataFromFile();
-            }
-        });
-
+    //解析
+    private static void initJX(String data){
+        tv_show.setText(data);
     }
 
     /**
-     * 按行读取文件内容
-     *
-     * @param path
-     * @return
+     * 或取本机的ip地址
      */
-    public static StringBuilder pickDataLine(String path) {
-        File file = new File(path);
-        if (!file.exists()) {
-            Log.d("FileUtils", "file doesn't exists");
-            return null;
-        }
-        InputStreamReader reader = null;
-        BufferedReader br = null;
-        StringBuilder sb = new StringBuilder();
-        try {
-            reader = new InputStreamReader(new FileInputStream(file));
-            br = new BufferedReader(reader);
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                if (line.contains("DATA-USAGE")) {
-                    Log.d("FileUtils", line);
-                    String tmp = (line.substring(0,23) + " " + line.substring(line.indexOf("DATA-USAGE")));
-                    sb.append(tmp);
-                    sb.append("\n");
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-                if (br != null) {
-                    br.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sb;
-    }
-
-    protected void pickDataFromFile() {
-//        String sdPath = Environment.getExternalStorageDirectory().getAbsolutePath();
-        File dirPath = new File("/sdcard/scrm/picks/");
-        File[] files = dirPath.listFiles();
-        //如果为空对象，或为空文件夹，则直接删除文件夹
-        if (files == null || files.length == 0) {
-            return;
-        }
-        File targetFile = new File ("/sdcard/scrm/target.txt");
-        if (targetFile.exists()) {
-            targetFile.delete();
-        }
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(targetFile);
-            for (File itemFile : files) {
-                StringBuilder sb = pickDataLine(itemFile.getAbsolutePath());
-                fos.write(sb.toString().getBytes());
-            }
-            fos.flush();
-            fos.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private String getlocalip(){
+        WifiManager wifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ipAddress = wifiInfo.getIpAddress();
+        //  Log.d(Tag, "int ip "+ipAddress);
+        if(ipAddress==0)return null;
+        return ((ipAddress & 0xff)+"."+(ipAddress>>8 & 0xff)+"."
+                +(ipAddress>>16 & 0xff)+"."+(ipAddress>>24 & 0xff));
     }
 
 }
